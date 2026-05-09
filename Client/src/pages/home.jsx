26a7c59ca478a28637b95f8dd6e8b6be86f90d0a
Suyash <mishrasuyash013@gmail.com>
2026-05-09 12:33:05 +0530

    Deployment Fix

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -47,6 +47,8 @@ function Home() {
                     </p>
                 </div>
 
+                
+
                 <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
                     {product.map((item) => (
                         <div
